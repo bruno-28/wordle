43f52729120a4955a60a4df7633d241579dd6a83
bruno-28 <bapn.14@gmail.com>
2022-10-20 15:10:16 +0200

    Replace local data with public api Update Keypad to use local array instead of db.json fetch

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import Wordle from "./components/Wordle";
 function App() {
   const [solution, setSolution] = useState(null);
 
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/solutions")
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       const randomSolution = json[Math.floor(Math.random() * json.length)];
+  //       setSolution(randomSolution.word);
+  //     });
+  // }, [setSolution]);
+
   useEffect(() => {
-    fetch("http://localhost:3001/solutions")
+    fetch("https://random-word-api.herokuapp.com/word?length=5")
       .then((res) => res.json())
       .then((json) => {
-        const randomSolution = json[Math.floor(Math.random() * json.length)];
-        setSolution(randomSolution.word);
+        setSolution(json[0]);
       });
   }, [setSolution]);
 
