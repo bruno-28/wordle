78ec4e838c2f0c29b4efc04d30c6310c6bf02327
bruno-28 <bapn.14@gmail.com>
2022-10-20 16:48:19 +0200

    Add button to close modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Modal({ isCorrect, turn, solution }) {
+  const [isModalClosed, setIsModalClosed] = useState(false);
+
+  const handleClick = () => {
+    setIsModalClosed(true);
+  };
+
   return (
-    <div className="modal">
-      <div>
-        {isCorrect ? <h1>You won!</h1> : <h1>You lost!</h1>}
-        <p>The word was:</p>
-        <p className="solution">{solution}</p>
-        {isCorrect && <p>You found the solution in {turn} guesses!</p>}
-        <p>Play again?</p>
-      </div>
+    <div>
+      {!isModalClosed && (
+        <div className="modal">
+          <div>
+            <button className="close-modal" onClick={handleClick}>
+              X
+            </button>
+            {isCorrect ? <h1>You won!</h1> : <h1>You lost!</h1>}
+            <p>The word was:</p>
+            <p className="solution">{solution}</p>
+            {isCorrect && <p>You found the solution in {turn} guesses!</p>}
+            <p>Play again?</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
